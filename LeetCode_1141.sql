7df82f779a14640c0582606a752d628aa78d0180
Prithviraj Chavan <prithvirajchavn35@gmail.com>
2025-06-19 22:06:45 +0530

    Update LeetCode_1141.sql

--- LeetCode_1141.sql
+++ LeetCode_1141.sql
@@ -1,3 +1,5 @@
+-- Link: https://leetcode.com/problems/user-activity-for-the-past-30-days-i/?envType=study-plan-v2&envId=top-sql-50
+
 select activity_date as day, count(distinct(user_id)) as active_users from Activity 
 where
     activity_date between '2019-06-28' and '2019-07-27'
