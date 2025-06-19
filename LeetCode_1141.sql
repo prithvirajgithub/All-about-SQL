e46b51aee7dcbc2dba54664a6d60f7bca9f7c426
Prithviraj Chavan <prithvirajchavn35@gmail.com>
2025-06-19 22:01:01 +0530

    Update LeetCode_1141.sql

--- LeetCode_1141.sql
+++ LeetCode_1141.sql
@@ -2,6 +2,6 @@ select activity_date as day, count(distinct(user_id)) as active_users from Activ
 where
     activity_date between '2019-06-28' and '2019-07-27'
 group by
-    activity_date,activity_date
+    activity_date
 order by
     activity_date
